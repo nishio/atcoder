a44b4a74aa47a42274c173547e9aea87c6997d6b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-06-23 18:18:12 +0900

    add -f, -t, print detail

--- testall.py
+++ testall.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
+"""
+utility to test all input/output
 
+It assumes in/x.txt and out/x.txt are paired.
+If you download testcases from atcoder's official Dropbox and unzip, it satisfied.
+"""
 import sys
 import subprocess
 import os
 import time
+import argparse
 
-print(sys.argv)
-if len(sys.argv) == 1:
-    target = "main.py"
+parser = argparse.ArgumentParser(description='Test all')
+parser.add_argument('target_script', type=str)
+parser.add_argument(
+    "-f", "--force-all", action="store_true",
+    help="even if some test fail, run all")
+parser.add_argument(
+    "-t", "--test", action="store",
+    help="specify testcase filename")
+
+args = parser.parse_args()
+
+target = args.target_script
+max_time = 0
+num_ac = 0
+num_wa = 0
+num_re = 0
+if args.test:
+    test = [x.replace("in/", "") for x in args.test.split(",")]
 else:
-    target = sys.argv[1]
+    test = os.listdir("in")
 
-for f in os.listdir("in"):
+for f in test:
     print(f"test: {f}", end="\t")
     t = time.perf_counter()
     ret = subprocess.call(
@@ -19,9 +40,25 @@ for f in os.listdir("in"):
     t = time.perf_counter() - t
     if ret:
         print("RE")
-        break
+        num_re += 1
+        if not args.force_all:
+            break
+        continue
     ret = subprocess.call(f"diff output out/{f}", shell=True)
     if ret:
         print("WA")
-        break
+        num_wa += 1
+        if not args.force_all:
+            break
+        continue
     print(f"AC {t:.3f}")
+    num_ac += 1
+    max_time = max(max_time, t)
+
+msg = f"AC: {num_ac}"
+if num_wa:
+    msg += f", WA: {num_wa}"
+if num_re:
+    msg += f", RE: {num_re}"
+msg += f", max_time: {max_time:.2f}"
+print(msg)
