eea46623d06ef86be20969fc9a5fe25d096d12a1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-06-26 21:18:10 +0900

    working

--- agc044/B/b.py
+++ agc044/B/b.py
@@ -1,65 +1,46 @@
 #!/usr/bin/env python3
-import pprint
 from collections import defaultdict
+import pprint
+import numpy as np
+from heapq import heappush, heappop
 N = int(input())
-edges = defaultdict(set)
-for i in range(1, N * N + 1):
-    e = []
-    if i <= N:
-        e.append(0)
-    else:
-        e.append(i - N)
-
-    if i % N == 0:
-        e.append(0)
-    else:
-        e.append(i + 1)
-
-    if i % N == 1:
-        e.append(0)
-    else:
-        e.append(i - 1)
-
-    if i > N * (N - 1):
-        e.append(0)
-    else:
-        e.append(i + N)
-    e = set(e)
-    for v in e:
-        edges[v].add(i)
-        edges[i].add(v)
-
-# print(edges)
-vcost = {0: -1}
-front = [0]
-cost = 0
-while front:
-    newFront = []
-    for p in front:
-        for n in edges[p]:
-            if n not in vcost:
-                vcost[n] = cost
-                newFront.append(n)
-    front = newFront
-    cost += 1
-
-# print(vcost)
-
-
-def update(xs, newcost):
-    for x in xs:
-        if vcost[x] > newcost:
-            vcost[x] = newcost
-            update(edges[x], newcost + 1)
-
-
 PS = [int(x) for x in input().split()]
-cost = 0
-for p in PS:
-    cost += vcost[p]
-    newcost = min(vcost[i] for i in edges[p]) + 1
-    update(edges[p], newcost)
-    for n in edges[p]:
-        edges[n].update(edges[p])
 
-print(cost)
+
+person = [1] * (N * N)
+
+
+def search(start):
+    to_visit = [start]
+    next_visit = []
+    visited = [0] * (N * N)
+    cost = 0
+    while to_visit:
+        while to_visit:
+            p = to_visit.pop()
+            if visited[p]:
+                continue
+            visited[p] = 1
+
+            y, x = divmod(p, N)
+            if x == 0 or x == (N - 1) or y == 0 or y == (N - 1):
+                return cost
+            for d in (-1, +1, -N, +N):
+                if person[p + d]:
+                    next_visit.append(p + d)
+                else:
+                    to_visit.append(p + d)
+        to_visit = next_visit
+        next_visit = []
+        cost += 1
+
+
+total_cost = 0
+for p in PS:
+    # print(p)
+    p -= 1  # 0-origin
+    total_cost += search(p)
+    person[p] = 0
+    # print("t", total_cost)
+
+print(total_cost)
