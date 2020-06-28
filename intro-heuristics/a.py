2570f09e44ff10b93b393aae2256a0e382068f89
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-06-28 21:22:01 +0900

    calc score

---
+++ intro-heuristics/a.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+from heapq import heappush, heappop
+import sys
+import numpy as np
+
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.buffer.readline
+INF = 10 ** 9 + 1  # sys.maxsize # float("inf")
+
+
+def debug(*x):
+    print(*x)
+
+
+def solve():
+    "void()"
+    pass
+
+
+def main():
+    D = int(input())
+    CS = list(map(int, input().split()))
+    S = np.int16(read().split())
+    S = S.reshape((D, 26))
+    print(S)
+    print(S[4, 0])
+
+    as_input(OUT)
+    answer = np.int16(read().split())
+    print(answer)
+
+    last = [-1] * 26
+    score = 0
+    for i in range(D):
+        j = answer[i] - 1
+        score += S[i, j]
+        last[j] = i
+        score -= sum(CS[j] * (i - last[j]) for j in range(26))
+    print(score)
+    # solve()
+
+
+T = """
+5
+86 90 69 51 2 96 71 47 88 34 45 46 89 34 31 38 97 84 41 80 14 4 50 83 7 82
+19771 12979 18912 10432 10544 12928 13403 3047 10527 9740 8100 92 2856 14730 1396 15905 6534 4650 11469 3628 8433 2994 10899 16396 18355 11424
+6674 17707 13855 16407 12232 2886 11908 1705 5000 1537 10440 10711 4917 10770 17272 15364 19277 18094 3929 3705 7169 6159 18683 15410 9092 4570
+6878 4239 19925 1799 375 9563 3445 5658 19857 11401 6997 6498 19933 3848 2426 2146 19745 16880 17773 18359 3921 14172 16730 11157 5439 256
+8633 15862 15303 10749 18499 7792 10317 5901 9395 11433 3514 3959 5202 19850 19469 9790 5653 784 18500 10552 17975 16615 7852 197 8471 7452
+19855 17918 7990 10572 4333 438 9140 9104 12622 4985 12319 4028 19922 12132 16259 17476 2976 547 19195 19830 16285 4806 4471 9457 2864 2192
+"""
+
+OUT = """
+1
+17
+13
+14
+13
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    as_input(T)
+    main()
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    global read, input
+    f = io.StringIO(s.strip())
+    input = f.readline
+    read = f.read
+
+
+USE_NUMBA = False
+if (USE_NUMBA and sys.argv[-1] == 'ONLINE_JUDGE') or sys.argv[-1] == '-c':
+    print("compiling")
+    from numba.pycc import CC
+    cc = CC('my_module')
+    cc.export('solve', solve.__doc__.strip().split()[0])(solve)
+    cc.compile()
+    exit()
+else:
+    input = sys.stdin.buffer.readline
+    read = sys.stdin.buffer.read
+
+    if (USE_NUMBA and sys.argv[-1] != '-p') or sys.argv[-1] == "--numba":
+        # -p: pure python mode
+        # if not -p, import compiled module
+        from my_module import solve  # pylint: disable=all
+    elif sys.argv[-1] == "-t":
+        _test()
+        sys.exit()
+    elif sys.argv[-1] != '-p' and len(sys.argv) == 2:
+        # input given as file
+        input_as_file = open(sys.argv[1])
+        input = input_as_file.buffer.readline
+        read = input_as_file.buffer.read
+
+    main()
