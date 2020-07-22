a151ea9cd490220318c3abd3972b9a1ea4ecb90e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-07-22 13:53:12 +0900

    update

--- generate_snippets.py
+++ generate_snippets.py
@@ -124,6 +124,7 @@ push("impnp", "import numpy as np")
 push("main", open(os.path.join(DIR, "snippets/main.py")).read())
 push("numbamain", open(os.path.join(DIR, "snippets/numbamain.py")).read())
 push("debug_indent", open(os.path.join(DIR, "snippets/debug_indent.py")).read())
+push("lazy_segtree", open(os.path.join(DIR, "snippets/lazy_segtree.py")).read())
 
 path = os.path.join(DIR, ".vscode/snippet.code-snippets")
 json.dump(snippets, open(path, "w"), indent=2)
