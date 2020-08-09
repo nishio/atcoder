99c37a045eee39632f0679952d21ed3c79b1861d
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-08-09 21:53:25 +0900

    AC

--- agc047/a.py
+++ agc047/a.py
@@ -47,7 +47,6 @@ def solve1(N, AS):
 def solve(N, AS):
     import numpy as np
     ret = 0
-    # P = [[0] * 14 for i in range(14)]
     P2 = []
     P5 = []
     P = np.zeros((20, 20), dtype=np.int)
@@ -62,6 +61,8 @@ def solve(N, AS):
         while x % 5 == 0:
             p5 += 1
             x //= 5
+        if p2 > 18:
+            p2 = 18
         P2.append(p2)
         P5.append(p5)
         P[p2, p5] += 1
@@ -130,6 +131,93 @@ TEST_T2 = """
 8
 """
 
+T3 = """
+12
+1
+5
+25
+125
+625
+3125
+1.0
+0.2
+0.04
+0.008
+0.0016
+0.00032
+"""
+TEST_T3 = """
+>>> as_input(T3)
+>>> main()
+36
+"""
+
+T4 = """
+2
+1
+1.0
+"""
+TEST_T4 = """
+>>> as_input(T4)
+>>> main()
+1
+"""
+
+T5 = """
+4
+1
+5
+1.0
+0.2
+"""
+TEST_T5 = """
+>>> as_input(T5)
+>>> main()
+4
+"""
+
+T6 = """
+6
+1
+5
+25
+1.0
+0.2
+0.04
+"""
+TEST_T6 = """
+>>> as_input(T6)
+>>> main()
+9
+"""
+
+T7 = """
+8
+1
+5
+25
+125
+1.0
+0.2
+0.04
+0.008
+"""
+TEST_T7 = """
+>>> as_input(T7)
+>>> main()
+16
+"""
+
+T8 = """
+1
+8192
+"""
+TEST_T8 = """
+>>> as_input(T8)
+>>> main()
+0
+"""
+
 
 def _test():
     import doctest
@@ -137,7 +225,7 @@ def _test():
     g = globals()
     for k in sorted(g):
         if k.startswith("TEST_"):
-            doctest.run_docstring_examples(g[k], g)
+            doctest.run_docstring_examples(g[k], g, name=k)
 
 
 def as_input(s):
