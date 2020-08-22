4d731e72f414ca6614231dee0145afdf3a95dbc6
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-08-22 22:03:22 +0900

    2 TLE

--- abc176/d.py
+++ abc176/d.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np
 sys.setrecursionlimit(10**6)
 INF = 10 ** 9 + 1  # sys.maxsize # float("inf")
 MOD = 10 ** 9 + 7
@@ -11,7 +10,7 @@ def debug(*x):
 
 
 def solve(H, W, Ch, Cw, Dh, Dw, data):
-    data = list(data.reshape(-1))
+    # data = list(data.reshape(-1))
     HEIGHT = H + 4
     WIDTH = W + 4
     N = HEIGHT * WIDTH
@@ -65,10 +64,18 @@ def main():
     H, W = map(int, input().split())
     Ch, Cw = map(int, input().split())
     Dh, Dw = map(int, input().split())
-    data = np.array(list(read().strip()) + [0])
-    data = np.equal(data, 46).reshape((H, W + 1))
-    D = np.zeros((H + 4, W + 4), dtype=np.int8)
-    D[2:2+H, 2:2+W+1] = data
+    D = [0] * ((H + 4) * (W + 4))
+    for i in range(H):
+        S = input().strip()
+        for j in range(W):
+            D[(i + 2) * (W + 4) + (j + 2)] = 1 if S[j] == ord(b".") else 0
+    # print(D)
+    # for i in range(H + 4):
+    #     print(D[i * (H + 4): i * (H + 4) + (W + 4)])
+    # data = np.array(list(read().strip()) + [0])
+    # data = np.equal(data, 46).reshape((H, W + 1))
+    # D = np.zeros((H + 4, W + 4), dtype=np.int8)
+    # D[2:2+H, 2:2+W+1] = data
     # print(data)
     # print(D)
     print(solve(H, W, Ch, Cw, Dh, Dw, D))
