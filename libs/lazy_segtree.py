f5440b4205200cf9d0bf892f09f91023f2b82160
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-09-27 06:06:13 +0900

    moved

--- libs/lazy_segtree.py
+++ libs/lazy_segtree.py
@@ -1,3 +1,15 @@
+"""
+Lazy Segment Tree
+
+- Range update(set) Rande minimum
+- Range add, range sum
+- Rande add, range minimum
+- Rande update, rande sum
+- Range Affine Range Sum
+- Range nagate of 0/1 sequence, range calculation of inversion(TENTOUSUU)
+"""
+
+
 def set_depth(depth):
     global DEPTH, SEGTREE_SIZE, NONLEAF_SIZE
     DEPTH = depth
@@ -14,11 +26,11 @@ def get_size(pos):
     return (1 << (DEPTH - ret))
 
 
-def full_up(table, binop):
+def full_up(value_table, value_binop):
     for i in range(NONLEAF_SIZE - 1, 0, -1):
-        table[i] = binop(
-            table[2 * i],
-            table[2 * i + 1])
+        value_table[i] = value_binop(
+            value_table[2 * i],
+            value_table[2 * i + 1])
 
 
 def up(pos):
@@ -114,12 +126,6 @@ def lazy_range_update(
         action_table, value_table, R,
         action_composite, action_force, action_unity)
 
-    # print("action", file=sys.stderr)
-    # debugprint(action_table)
-    # print("value", file=sys.stderr)
-    # debugprint(value_table)
-    # print(file=sys.stderr)
-
     force_range_update(
         value_table, action_table, start, end,
         action, action_force, action_composite, action_unity)
@@ -144,6 +150,7 @@ def lazy_range_reduce(
 
 
 def debugprint(xs, minsize=0, maxsize=None):
+    import sys
     global DEPTH
     strs = [str(x) for x in xs]
     if maxsize != None:
@@ -173,6 +180,11 @@ def init_from_values(values, value_binop, value_unity):
     value_table[NONLEAF_SIZE:NONLEAF_SIZE + len(values)] = values
     full_up(value_table, value_binop)
     return value_table
+
+
+def set_items(table, xs):
+    for i, x in enumerate(xs, NONLEAF_SIZE):
+        table[i] = x
 
 
 def usage():
@@ -207,3 +219,479 @@ def usage():
         action_composite, action_force, action_unity, value_binop, value_unity))
 
 # --- end of library ---
+
+
+def mainF():
+    """
+    Range update(set) Rande minimum
+
+    RMQ and RUQ
+    https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_F
+    """
+    N, Q = map(int, input().split())
+    set_width(N)
+
+    value_unity = (1 << 31) - 1
+    value_table = [value_unity] * SEGTREE_SIZE
+    action_unity = -1
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        if action == action_unity:
+            return value
+        return action
+
+    def action_composite(new_action, old_action):
+        if new_action != action_unity:
+            return new_action
+        return old_action
+
+    for _ in range(Q):
+        q, *args = map(int, input().split())
+        if q == 0:
+            # update
+            left, right, value = args
+            right += 1  # include right
+            lazy_range_update(
+                action_table, value_table, left, right, value,
+                action_composite, action_force, action_unity, min)
+        else:
+            # find
+            left, right = args
+            print(lazy_range_reduce(
+                action_table, value_table, left, right + 1,
+                action_composite, action_force, action_unity, min, value_unity))
+
+
+T1F = """
+3 5
+0 0 1 1
+0 1 2 3
+0 2 2 2
+1 0 2
+1 1 2
+"""
+TEST_T1F = """
+>>> as_input(T1F)
+>>> mainF()
+1
+2
+"""
+T2F = """
+1 3
+1 0 0
+0 0 0 5
+1 0 0
+"""
+TEST_T2F = """
+>>> as_input(T2F)
+>>> mainF()
+2147483647
+5
+"""
+T3F = """
+8 10
+0 1 6 5
+0 2 7 2
+0 2 5 7
+1 3 3
+1 2 4
+1 0 3
+1 5 7
+1 2 6
+0 3 7 9
+1 2 6
+"""
+TEST_T3F = """
+>>> as_input(T3F)
+>>> mainF()
+7
+7
+5
+2
+2
+7
+"""
+T4F = """
+15 3
+0 1 6 5
+0 2 7 2
+1 6 7
+"""
+TEST_T4F = """
+>>> as_input(T4F)
+>>> mainF()
+2
+"""
+
+
+def mainG():
+    """
+    Range add, range sum
+
+    RSQ and RAQ
+    https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_G
+    """
+    from operator import add
+    # parse input
+    N, Q = map(int, input().split())
+    set_width(N)
+
+    value_unity = 0
+    value_table = [value_unity] * SEGTREE_SIZE
+    value_binop = add
+    action_unity = 0
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        return action * size + value
+
+    def action_composite(new_action, old_action):
+        return new_action + old_action
+
+    for _ in range(Q):
+        q, *args = map(int, input().split())
+        if q == 0:
+            # add
+            s, t, value = args
+            s -= 1
+            lazy_range_update(
+                action_table, value_table, s, t, value,
+                action_composite, action_force, action_unity, value_binop)
+        else:
+            # getSum
+            s, t = args
+            print(lazy_range_reduce(
+                action_table, value_table, s - 1, t,
+                action_composite, action_force, action_unity, value_binop, value_unity))
+
+
+T1G = """
+3 5
+0 1 2 1
+0 2 3 2
+0 3 3 3
+1 1 2
+1 2 3
+"""
+TEST_T1G = """
+>>> as_input(T1G)
+>>> mainG()
+4
+8
+"""
+T2G = """
+4 3
+1 1 4
+0 1 4 1
+1 1 4
+"""
+TEST_T2G = """
+>>> as_input(T2G)
+>>> mainG()
+0
+4
+"""
+
+
+def mainH():
+    """
+    Rande add, range minimum
+
+    RMQ and RAQ
+    https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_H
+    """
+    # parse input
+    N, Q = map(int, input().split())
+    set_width(N)
+
+    value_unity = 10 ** 9
+    value_table = [value_unity] * SEGTREE_SIZE
+    set_items(value_table, [0] * N)
+    full_up(value_table, min)
+
+    value_binop = min
+    action_unity = 0
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        return action + value
+
+    def action_composite(new_action, old_action):
+        return new_action + old_action
+
+    for _ in range(Q):
+        q, *args = map(int, input().split())
+        if q == 0:
+            # add
+            s, t, value = args
+            t += 1
+            lazy_range_update(
+                action_table, value_table, s, t, value,
+                action_composite, action_force, action_unity, value_binop)
+        else:
+            # getSum
+            s, t = args
+            print(lazy_range_reduce(
+                action_table, value_table, s, t + 1,
+                action_composite, action_force, action_unity, value_binop, value_unity))
+
+
+T1H = """
+6 7
+0 1 3 1
+0 2 4 -2
+1 0 5
+1 0 1
+0 3 5 3
+1 3 4
+1 0 5
+"""
+TEST_T1H = """
+>>> as_input(T1H)
+>>> mainH()
+-2
+0
+1
+-1
+"""
+
+
+def mainI():
+    """
+    Rande update, rande sum
+    RSQ and RUQ
+    https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_I
+    """
+    # parse input
+    from operator import add
+    N, Q = map(int, input().split())
+    set_width(N)
+
+    value_unity = 0
+    value_table = [0] * SEGTREE_SIZE
+    value_binop = add
+    action_unity = None
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        if action == action_unity:
+            return value
+        return action * size
+
+    def action_composite(new_action, old_action):
+        if new_action != action_unity:
+            return new_action
+        return old_action
+
+    for _ in range(Q):
+        q, *args = map(int, input().split())
+        if q == 0:
+            # update
+            s, t, value = args
+            t += 1
+            lazy_range_update(
+                action_table, value_table, s, t, value,
+                action_composite, action_force, action_unity, value_binop)
+        else:
+            # getSum
+            s, t = args
+            print(lazy_range_reduce(
+                action_table, value_table, s, t + 1,
+                action_composite, action_force, action_unity, value_binop, value_unity))
+
+
+T1I = """
+6 7
+0 1 3 1
+0 2 4 -2
+1 0 5
+1 0 1
+0 3 5 3
+1 3 4
+1 0 5
+"""
+TEST_T1I = """
+>>> as_input(T1I)
+>>> mainI()
+-5
+1
+6
+8
+"""
+
+
+def mainACLPC_K():
+    """
+    Range Affine Range Sum
+    https://atcoder.jp/contests/practice2/tasks/practice2_k
+
+    Pack 2 values into an integer to avoid TLE
+    """
+    MOD = 998244353
+    N, Q = map(int, input().split())
+    AS = list(map(int, input().split()))
+    set_width(N + 1)  # include N
+
+    value_unity = 0
+    value_table = [value_unity] * SEGTREE_SIZE
+    value_table[NONLEAF_SIZE:NONLEAF_SIZE + len(AS)] = AS
+
+    action_unity = None
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        if action == action_unity:
+            return value
+        # b, c = action
+        b = action >> 32
+        c = action - (b << 32)
+        return (value * b + c * size) % MOD
+
+    def action_composite(new_action, old_action):
+        if new_action == action_unity:
+            return old_action
+        if old_action == action_unity:
+            return new_action
+        b1 = old_action >> 32
+        c1 = old_action - (b1 << 32)
+        # b1, c1 = old_action
+        # b2, c2 = new_action
+        b2 = new_action >> 32
+        c2 = new_action - (b2 << 32)
+        b = (b1 * b2) % MOD
+        c = (b2 * c1 + c2) % MOD
+        return (b << 32) + c
+
+    def value_binop(a, b):
+        return (a + b) % MOD
+    full_up(value_table, value_binop)
+
+    for _q in range(Q):
+        q, *args = map(int, input().split())
+        if q == 0:
+            l, r, b, c = args
+            lazy_range_update(
+                action_table, value_table, l, r, ((b << 32) + c),
+                action_composite, action_force, action_unity, value_binop)
+        else:
+            l, r = args
+            print(lazy_range_reduce(
+                action_table, value_table, l, r,
+                action_composite, action_force, action_unity, value_binop, value_unity))
+
+
+TACLPC_K = """
+5 7
+1 2 3 4 5
+1 0 5
+0 2 4 100 101
+1 0 3
+0 1 3 102 103
+1 2 5
+0 2 5 104 105
+1 0 5
+"""
+TEST_TACLPC_K = """
+>>> as_input(TACLPC_K)
+>>> mainACLPC_K()
+15
+404
+41511
+4317767
+"""
+
+
+def mainACLPC_L():
+    """
+    Range nagate of 0/1 sequence, range calculation of inversion
+
+    https://atcoder.jp/contests/practice2/tasks/practice2_l
+    """
+    N, Q = map(int, input().split())
+    AS = list(map(int, input().split()))
+    set_width(N)  # include N
+
+    value_unity = (0, 0, 0)
+    value_table = [value_unity] * SEGTREE_SIZE
+    value_table[NONLEAF_SIZE:NONLEAF_SIZE +
+                len(AS)] = [(0, 1, 0) if a else (1, 0, 0) for a in AS]
+
+    def value_binop(a, b):
+        x1, y1, z1 = a
+        x2, y2, z2 = b
+        return (x1 + x2, y1 + y2, z1 + z2 + y1 * x2)
+    full_up(value_table, value_binop)
+
+    action_unity = False
+    action_table = [action_unity] * SEGTREE_SIZE
+
+    def action_force(action, value, size):
+        if action == action_unity:
+            return value
+        x, y, z = value
+        return (y, x, x * y - z)
+
+    def action_composite(new_action, old_action):
+        return new_action ^ old_action
+
+    for _q in range(Q):
+        q, *args = map(int, input().split())
+        if q == 1:
+            l, r = args
+            l -= 1  # 1-origin, r inclusive
+            lazy_range_update(
+                action_table, value_table, l, r, True,
+                action_composite, action_force, action_unity, value_binop)
+        else:
+            l, r = args
+            l -= 1  # 1-origin, r inclusive
+            print(lazy_range_reduce(
+                action_table, value_table, l, r,
+                action_composite, action_force, action_unity, value_binop, value_unity)[2])
+
+
+TACLPC_L = """
+5 5
+0 1 0 0 1
+2 1 5
+1 3 4
+2 2 5
+1 1 3
+2 1 2
+"""
+TEST_TACLPC_L = """
+>>> as_input(TACLPC_L)
+>>> mainACLPC_L()
+2
+0
+1
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    g = globals()
+    for k in sorted(g):
+        if k.startswith("TEST_"):
+            doctest.run_docstring_examples(g[k], g)
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    g = globals()
+    f = io.StringIO(s.strip())
+
+    g["input"] = lambda: bytes(f.readline(), "ascii")
+    g["read"] = lambda: bytes(f.read(), "ascii")
+
+
+if __name__ == "__main__":
+    import sys
+    if sys.argv[-1] == "-t":
+        print("testing")
+        _test()
+        sys.exit()
+    mainF()
