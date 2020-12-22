912b4701aeaf9fc6bff6f9da12b3292287b900d7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-12-22 16:14:59 +0900

    21WA

---
+++ PAST1/m.py
@@ -0,0 +1,112 @@
+# included from snippets/main.py
+
+def debug(*x, msg=""):
+    import sys
+    print(msg, *x, file=sys.stderr)
+
+
+def solve(N, M, AS, BS, CS, DS):
+    left = 1
+    right = 100000
+    while left * 1.00000001 < right:
+        x = (left + right) / 2
+        y = max(DS[i] - x * CS[i] for i in range(M))
+        zs = list(sorted([BS[i] - x * AS[i] for i in range(N)], reverse=True))
+        if y > 0:
+            y = y + sum(zs[:4])
+        else:
+            y = sum(zs[:5])
+
+        if y > 0:  # (2)
+            left = x
+        else:
+            right = x
+    return right
+
+
+def main():
+    N, M = map(int, input().split())
+    AS = []
+    BS = []
+    CS = []
+    DS = []
+    for _n in range(N):
+        a, b = map(int, input().split())
+        AS.append(a)
+        BS.append(b)
+    for _m in range(M):
+        c, d = map(int, input().split())
+        CS.append(c)
+        DS.append(d)
+
+    print(solve(N, M, AS, BS, CS, DS))
+
+
+# tests
+T1 = """
+6 2
+10 30
+20 60
+10 10
+30 100
+50 140
+40 120
+10 3
+30 1
+"""
+TEST_T1 = """
+>>> as_input(T1)
+>>> main()
+3.000000003633886
+"""
+
+T2 = """
+6 2
+1 20
+1 3
+32 100
+1 1
+1 2
+2 5
+10 100
+96 874
+"""
+TEST_T2 = """
+>>> as_input(T2)
+>>> main()
+9.000000014535544
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    g = globals()
+    for k in sorted(g):
+        if k.startswith("TEST_"):
+            print(k)
+            doctest.run_docstring_examples(g[k], g, name=k)
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    f = io.StringIO(s.strip())
+    g = globals()
+    g["input"] = lambda: bytes(f.readline(), "ascii")
+    g["read"] = lambda: bytes(f.read(), "ascii")
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.buffer.readline
+    read = sys.stdin.buffer.read
+    sys.setrecursionlimit(10 ** 6)
+    if sys.argv[-1] == "-t":
+        print("testing")
+        _test()
+        sys.exit()
+    main()
+    sys.exit()
+
+# end of snippets/main.py
