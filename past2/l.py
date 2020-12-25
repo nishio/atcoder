5d1e2887eab2ea8034b06ac288383677c15dc1a3
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-12-25 19:33:47 +0900

    sample AC

---
+++ past2/l.py
@@ -0,0 +1,102 @@
+# included from snippets/main.py
+
+def debug(*x, msg=""):
+    import sys
+    print(msg, *x, file=sys.stderr)
+
+
+def solve(N, K, D, AS):
+    end = N - D * (K - 1)
+    start = 0
+    ret = []
+    for _i in range(K):
+        subseq = AS[start:end]
+        if not subseq:
+            return [-1]
+        v = min(subseq)
+        ret.append(v)
+        start = AS.index(v, start) + D
+        end += D
+    return ret
+
+
+def main():
+    N, K, D = map(int, input().split())
+    AS = list(map(int, input().split()))
+    print(*solve(N, K, D, AS))
+
+
+# tests
+T1 = """
+3 2 2
+3 1 4
+"""
+TEST_T1 = """
+>>> as_input(T1)
+>>> main()
+3 4
+"""
+
+T2 = """
+3 3 2
+3 1 4
+"""
+TEST_T2 = """
+>>> as_input(T2)
+>>> main()
+-1
+"""
+
+T3 = """
+3 2 1
+3 1 4
+"""
+TEST_T3 = """
+>>> as_input(T3)
+>>> main()
+1 4
+"""
+
+T4 = """
+4 2 2
+3 6 5 5
+"""
+TEST_T4 = """
+>>> as_input(T4)
+>>> main()
+3 5
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    g = globals()
+    for k in sorted(g):
+        if k.startswith("TEST_"):
+            print(k)
+            doctest.run_docstring_examples(g[k], g, name=k)
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    f = io.StringIO(s.strip())
+    g = globals()
+    g["input"] = lambda: bytes(f.readline(), "ascii")
+    g["read"] = lambda: bytes(f.read(), "ascii")
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.buffer.readline
+    read = sys.stdin.buffer.read
+    sys.setrecursionlimit(10 ** 6)
+    if sys.argv[-1] == "-t":
+        print("testing")
+        _test()
+        sys.exit()
+    main()
+    sys.exit()
+
+# end of snippets/main.py
