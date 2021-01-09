a4813dc973964fbf1da25a43e472014620671a70
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-01-09 14:56:48 +0900

    4TLE

--- abc174/f.py
+++ abc174/f.py
@@ -14,10 +14,69 @@ def solve(SOLVE_PARAMS):
 
 
 def main():
+    import bisect
     # parse input
-    print(solve(SOLVE_PARAMS))
+    N, Q = map(int, input().split())
+    CS = list(map(int, input().split()))
+    XS = [[] for _i in range(N + 1)]
+    for i, c in enumerate(CS):
+        XS[c].append(i)
+
+    for _q in range(Q):
+        L, R = map(int, input().split())
+        ret = 0
+        for c in range(1, N + 1):
+            lc = bisect.bisect_left(XS[c], L - 1)
+            rc = bisect.bisect_right(XS[c], R - 1)
+            if lc != rc:
+                ret += 1
+        print(ret)
+
 
 # tests
+T1 = """
+4 3
+1 2 1 3
+1 3
+2 4
+3 3
+"""
+TEST_T1 = """
+>>> as_input(T1)
+>>> main()
+2
+3
+1
+"""
+
+T2 = """
+10 10
+2 5 6 5 2 1 7 9 7 2
+5 5
+2 4
+6 7
+2 2
+7 8
+7 9
+1 8
+6 9
+8 10
+6 8
+"""
+TEST_T2 = """
+>>> as_input(T2)
+>>> main()
+1
+2
+2
+1
+2
+2
+6
+3
+3
+3
+"""
 
 
 def _test():
