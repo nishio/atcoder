e1d1736e58e384813b46a06b7731afc084686f99
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-06 21:04:04 +0900

    AC

---
+++ abc191/a.py
@@ -0,0 +1,70 @@
+# included from snippets/main.py
+def debug(*x, msg=""):
+    import sys
+    print(msg, *x, file=sys.stderr)
+
+
+def solve(SOLVE_PARAMS):
+    pass
+
+
+def main():
+    V, T, S, D = map(int, input().split())
+    if not(V * T <= D <= V * S):
+        print("Yes")
+    else:
+        print("No")
+
+
+# tests
+T1 = """
+10 3 5 20
+"""
+TEST_T1 = """
+>>> as_input(T1)
+>>> main()
+Yes
+"""
+
+T2 = """
+10 3 5 30
+"""
+TEST_T2 = """
+>>> as_input(T2)
+>>> main()
+No
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    g = globals()
+    for k in sorted(g):
+        if k.startswith("TEST_"):
+            print(k)
+            doctest.run_docstring_examples(g[k], g, name=k)
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    f = io.StringIO(s.strip())
+    g = globals()
+    g["input"] = lambda: bytes(f.readline(), "ascii")
+    g["read"] = lambda: bytes(f.read(), "ascii")
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.buffer.readline
+    read = sys.stdin.buffer.read
+    sys.setrecursionlimit(10 ** 6)
+    if sys.argv[-1] == "-t":
+        print("testing")
+        _test()
+        sys.exit()
+    main()
+    sys.exit()
+
+# end of snippets/main.py
