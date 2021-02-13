69f8ff1af6d2a7ae0a3500f6d5f51c4afca68c9e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-13 21:45:33 +0900

    AC

---
+++ arc112/b.py
@@ -0,0 +1,175 @@
+# included from snippets/main.py
+def debug(*x, msg=""):
+    import sys
+    print(msg, *x, file=sys.stderr)
+
+
+def solve_WA(B, C):
+    # B-C/2 .. B
+    ret = 1 + C // 2
+    if C >= 1:
+        # -B-(C-1)/2 .. -B
+        ret += 1 + (C - 1) // 2
+        # -B .. -B + (C-1)/2
+        ret += (C - 1) // 2
+    if C >= 2:
+        # B .. B + (C-2)//2
+        ret += (C - 2) // 2
+    return ret
+
+
+def solve_WA2(B, C):
+    if B < 0:
+        B = -B
+    # B-C/2 .. B
+    s1 = B - C // 2
+    e1 = B
+    if C >= 1:
+        # -B-(C-1)/2 .. -B
+        s2 = -B - (C - 1) // 2
+        # -B .. -B + (C-1)/2
+        e2 = -B + (C - 1) // 2
+    if C >= 2:
+        # B .. B + (C-2)//2
+        e1 = B + (C - 2) // 2
+
+    ret = e1 - s1 + 1 + e2 - s2 + 1
+    if s1 <= e2:
+        # overlap
+        ret -= e2 - s1 + 1
+    return ret
+
+
+def solve(B, C):
+    # B-C/2 .. B
+    s1 = B - C // 2
+    e1 = B
+
+    if C >= 1:
+        # -B-(C-1)/2 .. -B
+        s2 = -B - (C - 1) // 2
+        # -B .. -B + (C-1)/2
+        e2 = -B + (C - 1) // 2
+    if C >= 2:
+        # B .. B + (C-2)//2
+        e1 = B + (C - 2) // 2
+
+    if C < 1:
+        return e1 - s1 + 1
+
+    # debug(s1, e1, s2, e2, msg=":s1,e1,s2,e2")
+    ret = e1 - s1 + 1 + e2 - s2 + 1
+    if s1 <= e2 and s2 <= e1:
+        # overlap
+        ret -= e2 - s1 + 1
+        if s1 < s2:
+            ret += s2 - s1
+        if e1 < e2:
+            ret += e2 - e1
+
+    return ret
+
+
+def blute(B, C):
+    from heapq import heappush, heappop
+    q = [(-C, B)]
+    used = set()
+    ret = set([B])
+    while q:
+        nc, b = heappop(q)
+        if nc > 0:
+            continue
+        ret.add(b)
+        if nc == 0:
+            continue
+        x = (nc + 1, -b)
+        if x not in used:
+            used.add(x)
+            heappush(q, x)
+        x = (nc + 2, b - 1)
+        if x not in used:
+            used.add(x)
+            heappush(q, x)
+    blute.ret = ret
+    return len(ret)
+
+
+def main():
+    B, C = map(int, input().split())
+    print(solve(B, C))
+
+
+def random_test():
+    for B in range(-10, 11):
+        for C in range(40):
+            if blute(B, C) != solve(B, C):
+                debug(B, C, solve(B, C), blute(B, C), msg=":B,C,solve,blute")
+
+
+# tests
+T1 = """
+11 2
+"""
+TEST_T1 = """
+>>> as_input(T1)
+>>> main()
+3
+"""
+T2 = """
+0 4
+"""
+TEST_T2 = """
+>>> as_input(T2)
+>>> main()
+4
+"""
+T3 = """
+112 20210213
+"""
+TEST_T3 = """
+>>> as_input(T3)
+>>> main()
+20210436
+"""
+T4 = """
+-211 1000000000000000000
+"""
+TEST_T4 = """
+>>> as_input(T4)
+>>> main()
+1000000000000000422
+"""
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    g = globals()
+    for k in sorted(g):
+        if k.startswith("TEST_"):
+            print(k)
+            doctest.run_docstring_examples(g[k], g, name=k)
+
+
+def as_input(s):
+    "use in test, use given string as input file"
+    import io
+    f = io.StringIO(s.strip())
+    g = globals()
+    g["input"] = lambda: bytes(f.readline(), "ascii")
+    g["read"] = lambda: bytes(f.read(), "ascii")
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.buffer.readline
+    read = sys.stdin.buffer.read
+    sys.setrecursionlimit(10 ** 6)
+    if sys.argv[-1] == "-t":
+        print("testing")
+        _test()
+        sys.exit()
+    main()
+    sys.exit()
+
+# end of snippets/main.py
